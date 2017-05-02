5cd29c8e2c3fa005bf171c6db22a35ba74eb806d
Zhang <zhzhang@othot.com>
2017-05-02 19:29:50 -0400

    use tf-idf for content based RS

--- content_based.py
+++ content_based.py
@@ -1,22 +1,43 @@
 import pandas as pd
-
+import numpy as np
+import random
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 # read item profiles
 cb = pd.read_csv("items.csv")
 cb.columns = ['item_id','feature_list']
 
-# generate feature dict
-all = []
-for i in xrange(len(cb)):
-    a = {}
-    features = cb.feature_list.ix[i].split()
-    for j in features:
-        a[j] = a.get(j, 0) + 1
-    all.append(a)
+# use tf-idf to extract features
+tf = TfidfVectorizer(analyzer='word', min_df=0)
+tfidf_matrix = tf.fit_transform(cb['feature_list'])
+feature_names = tf.get_feature_names()
+feature_df = pd.DataFrame(tfidf_matrix.toarray(), columns=feature_names)
 
-# create item profiles dataframe
-from sklearn.feature_extraction import DictVectorizer
-v = DictVectorizer(sparse=False)
-X = v.fit_transform(all)
-feature_names = v.get_feature_names()
-feature_df = pd.DataFrame(X, columns=feature_names)
+feature_df['item_id'] = cb['item_id']
+feature_df.drop('null',axis=1, inplace=True)
+items_unique = feature_df['item_id'].unique()
+
+# read purchase data
+df = pd.read_csv("purchases.csv")
+df.columns = ['qty', 'item_id', 'guest_id', 'purchase_date']
+df = df.dropna().reset_index(drop=True)
+df = df[df.qty > 0]
+df = df[['guest_id', 'item_id', 'qty']]
+df = df[df.guest_id.map(lambda x: x.isdigit()) & df.item_id.map(lambda x: x.isdigit())]
+
+# drop items that are not in item profiles
+df_short = df[df['item_id'].isin(items_unique)]
+
+# join feature profiles and purchase history
+all = pd.merge(df_short, feature_df, on='item_id', how='inner')
+
+# construct user profiles
+user_df = all.drop(['qty', 'item_id'], axis=1).groupby('guest_id').mean()
+
+# construct item profiles
+feature_df.index = feature_df['item_id']
+item_df = feature_df.drop('item_id', axis=1)
+
+# calculate similarity matrix for all user, item pairs
+cross_similarity = cosine_similarity(user_df, item_df)
